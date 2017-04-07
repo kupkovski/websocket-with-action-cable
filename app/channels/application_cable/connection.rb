a5717cc2601f8d26c616a01ca2a0fd27f98d65f3
Andre Kupkovski <kupkovski@gmail.com>
2017-04-06 22:21:03 -0300

    adds user identification

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,7 +3,18 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = 123456
+      self.current_user = find_verified_user
+    end
+
+    protected
+
+    def find_verified_user
+      verified_user = User.find_by(id: cookies.signed['user.id'])
+      if verified_user && cookies.signed['user.expires_at'] > Time.now
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
     end
   end
 end
